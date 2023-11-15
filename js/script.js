2f10e83fc7f46e278e3b085174d1c77a95fe1090
Nikolas82 <nikobogdan34@gmail.com>
2023-11-15 18:55:16 +0200

    add section reviews

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
 
@@ -79,6 +81,33 @@ var swiper = new Swiper(".vehicles-slider", {
 
 
 var swiper = new Swiper(".featured-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
   slidesPerView: 1,
   spaceBetween: 20,
   loop: true,
