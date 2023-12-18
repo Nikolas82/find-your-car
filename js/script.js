477b56269aa1fe22f787342fbd2ba9b06a6b0ebe
Nikolas82 <nikobogdan34@gmail.com>
2023-12-18 19:42:50 +0200

    new update

--- js/script.js
+++ js/script.js
@@ -1,68 +1,70 @@
-let menu = document.querySelector("#menu-btn");
-let navbar = document.querySelector(".navbar");
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-  menu.classList.toggle("fa-times");
-  navbar.classList.toggle("active");
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  };
+
 };
 
-document.querySelector("#login-btn").onclick = () => {
-  document.querySelector(".login-form-container").classList.toggle("active");
-};
+document.querySelector('.home').onmousemove = (e) =>{
 
-document.querySelector("#close-login-form").onclick = () => {
-  document.querySelector(".login-form-container").classList.remove("active");
-};
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
 
-window.onscroll = () => {
-  if (window.scrollY > 0) {
-    document.querySelector(".header").classList.add("active");
-  } else {
-    document.querySelector(".header").classList.remove("active");
-  }
-
-  menu.classList.remove("fa-times");
-  navbar.classList.remove("active");
-};
-
-window.onload = () => {
-  if (window.scrollY > 0) {
-    document.querySelector(".header").classList.add("active");
-  } else {
-    document.querySelector(".header").classList.remove("active");
-  }
-};
-
-document.querySelector(".home").onmousemove = (e) => {
-  document.querySelectorAll(".home-parallax").forEach((elm) => {
-    let speed = elm.getAttribute("data-speed");
+    let speed = elm.getAttribute('data-speed');
 
     let x = (window.innerWidth - e.pageX * speed) / 90;
     let y = (window.innerHeight - e.pageY * speed) / 90;
 
     elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+
   });
+
 };
 
-document.querySelector(".home").onmouseleave = () => {
-  document.querySelectorAll(".home-parallax").forEach((elm) => {
+
+document.querySelector('.home').onmouseleave = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
     elm.style.transform = `translateX(0px) translateY(0px)`;
+
   });
+
 };
 
 var swiper = new Swiper(".vehicles-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: true,
   grabCursor: true,
-  centeredSlides: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable:true,
   },
   breakpoints: {
     0: {
@@ -71,25 +73,24 @@ var swiper = new Swiper(".vehicles-slider", {
     768: {
       slidesPerView: 2,
     },
-    991: {
+    1024: {
       slidesPerView: 3,
     },
   },
 });
 
 var swiper = new Swiper(".featured-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: true,
   grabCursor: true,
-  centeredSlides: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable:true,
   },
   breakpoints: {
     0: {
@@ -98,25 +99,24 @@ var swiper = new Swiper(".featured-slider", {
     768: {
       slidesPerView: 2,
     },
-    991: {
+    1024: {
       slidesPerView: 3,
     },
   },
 });
 
-var swiper = new Swiper(".reviews-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: true,
+var swiper = new Swiper(".review-slider", {
   grabCursor: true,
-  centeredSlides: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable:true,
   },
   breakpoints: {
     0: {
@@ -125,7 +125,7 @@ var swiper = new Swiper(".reviews-slider", {
     768: {
       slidesPerView: 2,
     },
-    991: {
+    1024: {
       slidesPerView: 3,
     },
   },
